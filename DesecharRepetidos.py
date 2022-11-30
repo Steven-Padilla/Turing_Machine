6b7e3d576d69cff7180370f2e33f20397acd0fee
203423 <203423@ids.upchiapas.edu.mx>
2022-11-30 11:01:33 -0600

    update discard

--- DesecharRepetidos.py
+++ DesecharRepetidos.py
@@ -1,5 +1,5 @@
 import string
-
+import time
 blank='blank'
 right='right'
 left='left'
@@ -9,9 +9,11 @@ inicialState='q0'
 listAux=list(string.ascii_lowercase)
 result=[]
 outputTape=[]
-for i in range (200):
+inputTape2=[]
+for i in range (5000):
+    inputTape2.append(blank)
+for i in range (5000):
     outputTape.append(blank)
-
 transitions=[
     #( First block )  --->  (     Second block       )    
     ['q0','{',blank,blank,        'q1','{','{',blank,right,right,right],
@@ -28,7 +30,7 @@ def fillT():
     for i in range(26):
         for j in range(26):
             for k in range(26):
-                result.append(['q1',listAux[i],listAux[j],listAux[k],        'q2','&',listAux[j],listAux[k],right,static,static])
+                result.append(['q1',listAux[i],blank,blank,        'q2','&',listAux[i],listAux[i],right,static,static])
     for x in result:
         transitions.append(x)
     #Creating transitions q2 -> q2 with all the alphabet
@@ -36,23 +38,24 @@ def fillT():
     for i in range(26):
         for j in range(26):
             for k in range(26):
-                if listAux[i] != listAux[k] and listAux[j] != listAux[k]: 
+                if listAux[i] != listAux[k]: 
                     result.append(['q2',listAux[i],listAux[j],listAux[k],        'q2',listAux[i],listAux[j],listAux[k],right,static,static])
-                    result.append(['q2',',',listAux[j],listAux[k],        'q2',',',listAux[j],listAux[k],right,static,static])
+                    
                 elif listAux[i] == listAux[k]:
                     result.append(['q2',listAux[i],listAux[j],listAux[k],        'q2','!',listAux[j],listAux[k],right,static,static])
-    for x in result:
-        transitions.append(x)
-                    
+                result.append(['q2',',',listAux[j],listAux[k],        'q2',',',listAux[j],listAux[k],right,static,static]) 
+                result.append(['q2','}',listAux[j],listAux[k],        'q3','}',listAux[j],listAux[k],left,static,static])
+                result.append(['q2','!',listAux[j],listAux[k],        'q2','!',listAux[j],listAux[k],right,static,static]) 
+
     for x in result:
         transitions.append(x)
     #Creating transitions q2 -> q3 with all the alphabet
-    result=[]
-    for i in range(26):
-        for j in range(26):
-            result.append(['q2','}',listAux[j],listAux[k],        'q3','}',listAux[j],listAux[k],left,static,static])
-    for x in result:
-        transitions.append(x)
+    # result=[]
+    # for i in range(26):
+    #     for j in range(26):
+           
+    # for x in result:
+    #     transitions.append(x)
     #Creating transitions q3 -> q3 with all the alphabet
     result=[]
     for i in range(26):
@@ -61,49 +64,53 @@ def fillT():
                 result.append(['q3','!',listAux[j],listAux[k],        'q3','!',listAux[j],listAux[k],left,static,static])
                 result.append(['q3',',',listAux[j],listAux[k],        'q3',',',listAux[j],listAux[k],left,static,static])
                 result.append(['q3',listAux[i],listAux[j],listAux[k],        'q3',listAux[i],listAux[j],listAux[k],left,static,static])
+                result.append(['q3','&',listAux[j],listAux[k],        'q4','&',listAux[j],listAux[k],right,right,static])
     for x in result:
         transitions.append(x)
     #Creating transitions q3 -> q4 with all the alphabet
-    result=[]
-    for k in range(26):
-        for j in range(26):
-            result.append(['q3','&',listAux[j],listAux[k],        'q4','&',listAux[j],listAux[k],right,right,static])
-    for x in result:
-        transitions.append(x)
+    # result=[]
+    # for k in range(26):
+    #     for j in range(26):
+            
+    # for x in result:
+    #     transitions.append(x)
     #Creating transitions q4 -> q6 with all the alphabet
     result=[]
     for k in range(26):
         result.append(['q4',',',blank,listAux[k],        'q6',',',',',listAux[k],right,right,right])
-    for x in result:
-        transitions.append(x)
-    #Creating transitions q4 -> q5 with all the alphabet
-    result=[]
-    for k in range(26):
         result.append(['q4','}',blank,listAux[k],        'q5','}','}',listAux[k],right,right,right])
     for x in result:
         transitions.append(x)
+    #Creating transitions q4 -> q5 with all the alphabet
+    # result=[]
+    # for k in range(26):
+        
+    # for x in result:
+    #     transitions.append(x)
     #Creating transitions q6 -> q6 with all the alphabet
     result=[]
     for i in range(26):
         for j in range(26):
-            result.append(['q6','!',listAux[j],blank,        'q6','!',listAux[j],blank,right,static,static])
-            result.append(['q6',',',listAux[j],blank,        'q6',',',listAux[j],blank,right,static,static])
+            result.append(['q6','!',blank,blank,        'q6','!',listAux[j],blank,right,static,static])
+            result.append(['q6',',',blank,blank,        'q6',',',listAux[j],blank,right,static,static])
+            result.append(['q6',listAux[i],blank,blank,        'q2',listAux[i],listAux[i],listAux[i],right,static,static])
+            result.append(['q6','}',blank,blank,        'q5','}','}',listAux[k],right,right,right])
     for x in result:
         transitions.append(x)
-    #Creating transitions q6 -> q2 with all the alphabet
-    result=[]
-    for i in range(26):
-        for j in range(26):
-            for k in range(26):
-                result.append(['q6',listAux[i],listAux[j],blank,        'q2',listAux[i],listAux[j],listAux[k],right,static,static])
-    for x in result:
-        transitions.append(x)
+    #Creating transitions q6 -> q2,q with all the alphabet
+    # result=[]
+    # for i in range(26):
+    #     for j in range(26):
+    #         for k in range(26):
+                
+    # for x in result:
+    #     transitions.append(x)
     #Creating transitions q6 -> q5 with all the alphabet
-    result=[]
-    for k in range(26):
-        result.append(['q6','}',blank,listAux[k],        'q5','}','}',listAux[k],right,right,right])
-    for x in result:
-        transitions.append(x)
+    # result=[]
+    # for k in range(26):
+        
+    # for x in result:
+    #     transitions.append(x)
 
 def transportStr(tape):
     aux =[]
@@ -111,57 +118,66 @@ def transportStr(tape):
         aux.append(x)
     return aux
 
-def discard(inputTape):
-    inputTape=transportStr(inputTape)
+def turing_intersection(inputTape1):
+    inputTape1=transportStr(inputTape1)
     state=inicialState
     head1=0
     head2=0
     head3=0
-    inputTape.append(blank)
-    print(str(inputTape))
-    while len(inputTape)!=head1:
-        band=False
-        for singleT in transitions:
-            if singleT[0]== state and singleT[3]== outputTape[head3] and singleT==inputTape[head1]:
-                inputTape[head1]=singleT[5]
-                outputTape[head3]=singleT[7]
-                if singleT[8]==right:
-                    head1+=1
-                elif singleT[8]==left:
-                    head1-=1
-                if singleT[9]==right:
-                    head2+=1
-                elif singleT[9]==left:
-                    head2-=1
-                if singleT[10]==right:
-                    head3+=1
-                elif singleT[10]==left:
-                    head3-=1
-                state=singleT[4]
-                band=True
-                print(f'Estado:{state}')
-                print(f'Entrada:{inputTape}')
-                print(f'Salida:{outputTape}')
-        
-        if band==False:
-            break
+    inputTape1.append(blank)
+    inputTape2.append(blank)
+    print(str(inputTape1)+'\n'+str(inputTape2) )
+    while len(inputTape1) != head1 and len(inputTape2) !=head2: #Iterating each letter from the input
+        # for j in range(len(inputTape2)):
+            band=False
+            for singleT in transitions:
+                if singleT[0] == state  and singleT[3]==outputTape[head3] and singleT[1]==inputTape1[head1] and singleT[2]==inputTape2[head2] :
+                    inputTape1[head1]=singleT[5]
+                    inputTape2[head2]=singleT[6]
+                    outputTape[head3]=singleT[7]
+                    if singleT[8]==right:
+                        head1+=1
+                    if singleT[8]==left:
+                        head1-=1
+                    if singleT[9]==right:
+                        head2+=1
+                    if singleT[9]==left:
+                        head2-=1
+                    if singleT[10]==right:
+                        head3+=1
+                    if singleT[10]==left:
+                        head3-=1
+                    state=singleT[4]
+                    band=True
+                    print(f'Estado: {state}')
+                    print(f'Cabeza 1: {inputTape1[head1]}')
+                    print(f'Cabeza 2: {inputTape2[head2]}')
+                    print(f'Cabeza 3: {outputTape[head3]}')
+
+
+            if(band==False):
+                break
     if state==finalState:
         return True
     return False
+            
 
 def generatePDF():
-    file=open ('Transitions2.txt','w')
+    file=open ('Transitions.txt','w')
     for singleT in transitions:
         file.write(str(singleT)+',\n')
     file.close()
 
-    
 
 def main():
+
     fillT()
     generatePDF()
-    x="{a,b,c}"
-    accepted=discard(x)
+    # for x in transitions:
+    #     print(str(x)+',')
+    x="{a,c,f,g,v,a}"
+    y="{a,t,s,k,v}"
+    accepted=turing_intersection(x) #Write here your input {}#{}
     result=''
     for char in outputTape:
         if char!=blank:
@@ -169,9 +185,8 @@ def main():
         if char==blank:
             break
     if accepted:
-        print("cadena final: "+result)
+        print (f'Intersection of {x} with {y}: \n {result}')
     else:
-        print("cadena no aceptada")
-
+        print(f'String NOT Accepted: {result}')
+    
 main()
-            
