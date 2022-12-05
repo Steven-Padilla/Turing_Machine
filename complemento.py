9a769ea5a1d5da88da316b33d9683a18306c6376
alejandro mota <aamotadev@gmail.com>
2022-12-04 19:18:58 -0600

    Update complemento.py

--- complemento.py
+++ complemento.py
@@ -8,10 +8,7 @@ class Complemento():
         self.finalState=['q3','q5','q6']
         self.inicialState='q0'
         self.blank='blank'
-        self.outputTape=[]                     #Creating and filling outPutTape with blanks
-        for i in range (200):
-            self.outputTape.append(self.blank)
-        
+  
         self.transitions=[
         #( Firts block )  --->  (     Second block       )    
             ['q0','{','{',        'q1','{','{',self.right,self.right],  
@@ -53,14 +50,18 @@ class Complemento():
     #Creating transitions q4 -> q7 with all the alphabet
         result=[]
         for i in range(len(listAux)):
-            result.append(['q4',',',listAux[j],       'q7',',',listAux[j],self.right,self.static])
+            result.append(['q4',',',listAux[i],       'q7',',',listAux[i],self.right,self.static])
             self.transitions.append(result[i])
             
     #Creating transitions q7 -> q7 with all the alphabet
         result=[]
+        listComa = listAux.copy()
+        listComa.append(',')
         for i in range(len(listAux)):
-            result.append(['q7',listAux[i],listAux[j],       'q7',listAux[i],listAux[j],self.static,self.left])
-            self.transitions.append(result[i]) 
+            for j in range(len(listComa)):
+                result.append(['q7',listAux[i],listComa[j],       'q7',listAux[i],listComa[j],self.static,self.left])
+        for x in result:
+            self.transitions.append(x)
     
     #Creating transitions q7 -> q1 with all the alphabet
         result=[]
@@ -71,13 +72,13 @@ class Complemento():
     #Creating transitions q4 -> q5 with all the alphabet
         result=[]
         for i in range(len(listAux)):
-            result.append(['q4','}',listAux[j],        'q5','}',listAux[j],self.static,self.static])
+            result.append(['q4','}',listAux[i],        'q5','}',listAux[i],self.static,self.static])
             self.transitions.append(result[i])
             
     #Creating transitions q1 -> q6 with all the alphabet
         result=[]
         for i in range(len(listAux)):
-            result.append(['q1','}',listAux[j],        'q6','}',listAux[j],self.static,self.static])
+            result.append(['q1','}',listAux[i],        'q6','}',listAux[i],self.static,self.static])
             self.transitions.append(result[i])
             
     def transportStr(self,tape):
@@ -86,38 +87,36 @@ class Complemento():
             aux.append(x)
         return aux
 
-    def turingMachine(self,inputTape1,inputTape2):
+    def turingMachine(self,inputTape1, inputTape2):
         inputTape1=self.transportStr(inputTape1)
-        inputTape2=self.transportStr(inputTape2)       
+        inputTape2=self.transportStr(inputTape2)
         state=self.inicialState
         head1=0
         head2=0
-        head3=0
         inputTape1.append(self.blank)
         inputTape2.append(self.blank)
-        while len(inputTape1) != head1 and len(inputTape2) !=head2: #Iterating each letter from the input
-                band=False
-                for singleT in self.transitions:
-                    if singleT[0] == state  and singleT[3]==self.outputTape[head3] and singleT[1]==inputTape1[head1] and singleT[2]==inputTape2[head2] :
-                        inputTape1[head1]=singleT[5]
-                        inputTape2[head2]=singleT[6]
-                        self.outputTape[head3]=singleT[7]
-                        if singleT[8]==self.right:
-                            head1+=1
-                        if singleT[8]==self.left:
-                            head1-=1
-                        if singleT[9]==self.right:
-                            head2+=1
-                        if singleT[9]==self.left:
-                            head2-=1
-                        if singleT[10]==self.right:
-                            head3+=1
-                        if singleT[10]==self.left:
-                            head3-=1
-                        state=singleT[4]
-                        band=True
-                if(band==False):
-                    break
+        while len(inputTape1) != head1 and len(inputTape2) !=head2: #Iterating each letter from the inputng each letter from the input
+            band=False
+            for singleT in self.transitions:
+                if singleT[0] == state  and singleT[2]==inputTape2[head2] and singleT[1]==inputTape1[head1]:
+                    inputTape2[head2]=singleT[5]
+                    inputTape1[head1]=singleT[4]
+                    if singleT[6]==self.right:
+                        head1+=1
+                    if singleT[6]==self.left:
+                        head1-=1
+                    if singleT[7]==self.right:
+                        head2+=1
+                    if singleT[7]==self.left:
+                        head2-=1
+                    state=singleT[3]
+                    band=True
+                    print(f'state:{state}')
+                    print(f'head1:{inputTape1[head1]}')
+                    print(f'head2:{inputTape2[head2]}')
+
+            if(band==False):
+                break
         if state==self.finalState[1] or state==self.finalState[2]:
             return True
         return False
@@ -125,15 +124,9 @@ class Complemento():
     def ejecutar(self,conjunto,universo):
         self.fillT()
         accepted=self.turingMachine(conjunto,universo) #Write here your input {}#{}
-        result=''
-        for char in self.outputTape:
-            if char!=self.blank:
-                result+=char
-            if char==self.blank:
-                break
         if accepted:
-            return result
+            return True
         else:
             return False
 prueba = Complemento()
-print(prueba.ejecutar('{a,i,u}','{a,e,i,o,u}'))
+print(prueba.ejecutar('{a,e,i,o,u,a,a,u}','{a,e,i,o,u}'))
